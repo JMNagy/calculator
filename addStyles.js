2dedb88fa62fb37c4d7b8ee9be176accf8566ffd
John-Michael <johnmcnagy@gmail.com>
2022-08-08 00:10:00 -0400

    Finished styles via jscript addStyles.js

--- addStyles.js
+++ addStyles.js
@@ -8,7 +8,7 @@ const calcBody = document.querySelector(".calcBody");
 calcBodyClass.split(" ").forEach((cls) => calcBody.classList.add(cls));
 
 const outputClass =
-  "flex flex-col 2-3/4 h-12 justify-center items-end m-1 bg-gray-300 text-gray-900 font-semibold px-2 border border-black hover:border-gray-500 rounded";
+  "flex flex-col w-3/4 h-12 justify-center items-end m-1 bg-gray-300 text-gray-900 font-semibold px-2 border border-black hover:border-gray-500 rounded";
 const output = document.querySelector(".output");
 outputClass.split(" ").forEach((cls) => output.classList.add(cls));
 
@@ -32,8 +32,10 @@ buttons.forEach((btn) => {
 
 function addClasses(button) {
   const btnNumberClass = "hover:bg-green-500 border-green-500";
-  const btnClearClass = "hover:bg-red-500 border-red-500";
+  const btnClearClass = "hover:bg-orange-500 border-orange-500";
   const btnOpClass = "hover:bg-blue-500 border-blue-500";
+  const btnEqClass = "hover:bg-red-500 border-red-500";
+  const btnDecClass = "hover:bg-yellow-500 border-yellow-500";
 
   if (button.classList.value === "number") {
     btnNumberClass.split(" ").forEach((cls) => button.classList.add(cls));
@@ -41,6 +43,10 @@ function addClasses(button) {
     btnOpClass.split(" ").forEach((cls) => button.classList.add(cls));
   } else if (button.classList.value === "clear") {
     btnClearClass.split(" ").forEach((cls) => button.classList.add(cls));
+  } else if (button.classList.value === "equal") {
+    btnEqClass.split(" ").forEach((cls) => button.classList.add(cls));
+  } else if (button.classList.value === "decimal") {
+    btnDecClass.split(" ").forEach((cls) => button.classList.add(cls));
   }
 
   const baseClass =
